eae13db8145aa2e14e5cc8627e1cc8abac7229f1
AdamDuchna <95301576+AdamDuchna@users.noreply.github.com>
2021-12-04 20:47:24 +0100

    added test encode single letter

---
+++ src/Morse.py
@@ -0,0 +1,3 @@
+def encode(text):
+
+def decode(text):
--- tests/MorseTests.py
+++ tests/MorseTests.py
@@ -1,1 +1,7 @@
 import unittest
+from Morse import *
+class MorseEncodeTest(unittest.TestCase):
+    def setUp(self):
+        self.temp = Morse.encode()
+    def test_Morse_encode_single_letter(self):
+        self.assertEqual(".-",temp("A"))
