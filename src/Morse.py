1e42c429f150effdd9ab7370b48105b8af889f5f
AdamDuchna <95301576+AdamDuchna@users.noreply.github.com>
2021-12-04 21:59:31 +0100

    major refactoring in morse encoding lowercase word test passed

--- src/Morse.py
+++ src/Morse.py
@@ -1,11 +1,30 @@
 def encode(text):
-    if text == 'A':
-        return '.-'
-    if text == '1':
-        return '.----'
-    if text == '.':
-        return '.-.-.-'
     if type(text) != str:
         raise TypeError(text)
+    else:
+        text= text.upper()
+        morse_dict_encode = {'A': '.-', 'B': '-...',
+                             'C': '-.-.', 'D': '-..', 'E': '.',
+                             'F': '..-.', 'G': '--.', 'H': '....',
+                             'I': '..', 'J': '.---', 'K': '-.-',
+                             'L': '.-..', 'M': '--', 'N': '-.',
+                             'O': '---', 'P': '.--.', 'Q': '--.-',
+                             'R': '.-.', 'S': '...', 'T': '-',
+                             'U': '..-', 'V': '...-', 'W': '.--',
+                             'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                             '1': '.----', '2': '..---', '3': '...--',
+                             '4': '....-', '5': '.....', '6': '-....',
+                             '7': '--...', '8': '---..', '9': '----.',
+                             '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                             '?': '..--..', '/': '-..-.', '-': '-....-',
+                             '(': '-.--.', ')': '-.--.-'}
+        encoded_msg= ''
+        for letter in text:
+            if letter != ' ':
+                encoded_msg+=morse_dict_encode[letter] + ' '
+            else:
+                encoded_msg+= ' '
+        return encoded_msg[:-1]
+
 def decode(text):
     return text
