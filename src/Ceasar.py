69135429b9f753335270ea854ea5f05cd220fc49
AdamDuchna <95301576+AdamDuchna@users.noreply.github.com>
2021-12-06 19:04:50 +0100

    ceasar encoding for mixed cases

--- src/Ceasar.py
+++ src/Ceasar.py
@@ -1,11 +1,24 @@
 import re
+
+
 class Ceasar:
-    def encode(self,text):
+    def encode(self, text):
         if str == type(text):
-            if bool(re.match("[A-Za-z]+",text)):
-                answ=""
+            if bool(re.match("[A-Za-z]+", text)):
+                answ = ""
                 for letter in text:
-                    answ += chr(ord(letter) + 3)
+                    if letter == " ":
+                        answ+= " "
+                    elif letter.isupper():
+                        if ord(letter) + 3 > 90:
+                            answ += chr(64 + ((ord(letter) + 3) % 90))
+                        else:
+                            answ += chr(ord(letter) + 3)
+                    elif letter.islower():
+                        if ord(letter) + 3 > 122:
+                            answ += chr(96 + ((ord(letter) + 3) % 122))
+                        else:
+                            answ += chr(ord(letter) + 3)
                 return answ
             elif text == "":
                 return text
@@ -13,5 +26,6 @@ class Ceasar:
                 raise ValueError("Contains non-letters")
         else:
             raise TypeError(text)
-    def decode(self,text):
+
+    def decode(self, text):
         return text
