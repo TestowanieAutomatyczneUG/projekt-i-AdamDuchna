8174ec0a75cb47436626efefa0eeb97b71c9c5be
AdamDuchna <95301576+AdamDuchna@users.noreply.github.com>
2021-12-06 17:43:03 +0100

    added datatype tests to Ceasar

--- src/Ceasar.py
+++ src/Ceasar.py
@@ -1,5 +1,5 @@
 class Ceasar:
     def encode(self,text):
-        return text
+        return chr(ord(text)+3)
     def decode(self,text):
         return text
--- tests/CeasarTests.py
+++ tests/CeasarTests.py
@@ -1,13 +1,36 @@
 import unittest
-import pytest
+
 from src.Ceasar import Ceasar
+
 
 class CeasarEncodeTest(unittest.TestCase):
     def setUp(self):
         assistant = Ceasar()
         self.temp = assistant.encode
+
     def test_Ceasar_encode_single_uppercase_letter(self):
         self.assertEqual('D', self.temp('A'))
-    def test_Ceasar_encode_single_uowercase_letter(self):
+
+    def test_Ceasar_encode_single_lowercase_letter(self):
         self.assertEqual('f', self.temp('c'))
 
+    def test_Ceasar_encode_non_letter(self):
+        self.assertEqual(ValueError, self.temp('1'))
+
+    def test_Ceasar_encode_intiger(self):
+        self.assertRaises(TypeError, self.temp, 3)
+
+    def test_Ceasar_encode_list(self):
+        self.assertRaises(TypeError, self.temp, [1, 4])
+
+    def test_Ceasar_encode_double(self):
+        self.assertRaises(TypeError, self.temp, 2.65)
+
+    def test_Ceasar__object(self):
+        self.assertRaises(TypeError, self.temp, {})
+
+    def test_Ceasar_encode_tuple(self):
+        self.assertRaises(TypeError, self.temp, ())
+
+    def test_Ceasar_encode_boolean(self):
+        self.assertRaises(TypeError, self.temp, True)
