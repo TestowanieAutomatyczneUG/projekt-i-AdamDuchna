6b993651bb4ef41382553d285fbcdb843bb5a694
AdamDuchna <95301576+AdamDuchna@users.noreply.github.com>
2021-12-07 22:20:07 +0100

    type tests for affine encoding passed

--- tests/AnnifeTests.py
+++ tests/AnnifeTests.py
@@ -11,11 +11,32 @@ class AffineEncodeTest(unittest.TestCase):
     def test_Affine_encode_single_letter(self):
         self.assertEqual('F', self.temp('A',3,5))
 
-    def test_Ceasar_encode_single_lowercase_letter(self):
+    def test_Affine_encode_single_lowercase_letter(self):
         self.assertEqual('m', self.temp('c',5,2))
 
-    def test_Ceasar_encode_non_letter(self):
+    def test_Affine_encode_non_letter(self):
         self.assertRaises(ValueError, self.temp, '1',2,3)
+        
+    def test_Affine_encode_non_letter(self):
+        self.assertRaises(ValueError, self.temp, '1',2,3)
+
+    def test_Affine_encode_intiger(self):
+        self.assertRaises(TypeError, self.temp, 3,2,3)
+
+    def test_Affine_encode_list(self):
+        self.assertRaises(TypeError, self.temp, [1, 4],2,3)
+
+    def test_Affine_encode_double(self):
+        self.assertRaises(TypeError, self.temp, 2.65,2,3)
+
+    def test_Affine__object(self):
+        self.assertRaises(TypeError, self.temp, {},2,3)
+
+    def test_Affine_encode_tuple(self):
+        self.assertRaises(TypeError, self.temp, (),2,3)
+
+    def test_Affine_encode_boolean(self):
+        self.assertRaises(TypeError, self.temp, True,2,3)
 
 
 class AffineDecodeTest(unittest.TestCase):
