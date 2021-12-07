fa0c8026ce6b1abafa046fff415398a3c3e03449
AdamDuchna <95301576+AdamDuchna@users.noreply.github.com>
2021-12-07 22:08:47 +0100

    affine encode added simple tests

--- tests/AnnifeTests.py
+++ tests/AnnifeTests.py
@@ -6,10 +6,16 @@ from src.Affine import Affine
 class AffineEncodeTest(unittest.TestCase):
     def setUp(self):
         assist = Affine()
-        self.temp = assist.encode()
+        self.temp = assist.encode
 
     def test_Affine_encode_single_letter(self):
-        self.assertEqual('.-', self.temp('A'))
+        self.assertEqual('F', self.temp('A',3,5))
+
+    def test_Ceasar_encode_single_lowercase_letter(self):
+        self.assertEqual('m', self.temp('c',5,2))
+
+    def test_Ceasar_encode_non_letter(self):
+        self.assertRaises(ValueError, self.temp, '1')
 
 
 class AffineDecodeTest(unittest.TestCase):
